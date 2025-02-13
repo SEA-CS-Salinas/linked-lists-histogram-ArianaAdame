1c08d6700154154e03d40cb31dbc2a4d8d85f177
ArianaAdame <132471158+ArianaAdame@users.noreply.github.com>
2025-02-13 10:12:51 -0600

    Update HistoList.java

--- HistoList.java
+++ HistoList.java
@@ -1,3 +1,5 @@
+package linkedlistshisto;
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
@@ -6,75 +8,75 @@
 import java.util.*;
 import static java.lang.System.*;
 
-public class HistoList
-{
-   private HistoNode front;
+public class HistoList {
+	
+	private HistoNode front;
 
-	public HistoList( )
-	{
+	/* Non-parameterized constructor */
+	public HistoList() {
 		front = null;
 	}
-
-	//addLetter will add a new node to the front for let if let does not exist
-	//addLetter will bump up the count if let already exists
-	public void addLetter(char let)
-	{
-
-
-
-
-
-
-
-
+	
+	/* Adds a new node to the front with the value of let if it doesn't already exist in the list
+	 * If let already exists, then bump up the count 
+	 * @parma let		character to check for */
+	public void addLetter(char let) {
+		int temp = indexOf(let);
+		System.out.println(let + " " + temp);
+		/* let doesn't exist */
+		if (temp == -1) {
+			System.out.println("doesn't exist");
+			front = new HistoNode(let, 1, front);
+		}
+		/* let already exists */
+		else {
+			System.out.println("already exists");
+			HistoNode current = front;
+			for (int i = 0; i < temp; i++) {
+				current = current.getNext();
+			}
+			current.setLetterCount(current.getLetterCount() + 1);
+		}
 	}
 
-	//returns the index pos of let in the list if let exists
-	public int indexOf(char let)
-	{
-
-
-
-
-
-
-
-
-
+	/* Returns the index of let in the list if let exists, returns -1 otherwise
+	 * @param let		the character to search the index of
+	 * @return			position of let in the list, -1 if let doesn't exist */
+	/* while index != target, add to index count, return indexcount */
+	public int indexOf(char let) {
+		HistoNode current = front;
+		int indCount = 0;
+		while (current != null) {
+			if (current.getLetter() == let) {
+				return indCount;
+			}
+			current = current.getNext();
+			indCount++;
+		}
 		return -1;
 	}
 
-	//returns a reference to the node at spot
-	private HistoNode nodeAt(int spot)
-	{
-		HistoNode current=null;
-
-
-
-
-
-
-
-
-
-
+	/* Returns the node at the given index within the list
+	 *  @param spot		the index to check
+	 *  @return			the node at the given index */
+	private HistoNode nodeAt(int spot) {
+		HistoNode current = null;
+		for (int i = 0; i < spot; i++) {
+			current = front.getNext();
+		}
 		return current;
 	}
 
-	//returns a string will all values from list
-	public String toString()
-	{
+	
+	/* Prints the list
+	 * @return			string with all the values in the list */
+	public String toString() {
 		String output = "";
-
-
-
-
-
-
-
-
-
-
+		while (front != null) {
+			output +=  front.getLetter() + " - " + front.getLetterCount() + "\t";
+			front = front.getNext();
+		}
 		return output;
 	}
+	
 }
