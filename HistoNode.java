3ffb3b14e9a836bbdd69e4722a8a3b26797e87c1
ArianaAdame <132471158+ArianaAdame@users.noreply.github.com>
2025-02-13 10:13:16 -0600

    Update HistoNode.java

--- HistoNode.java
+++ HistoNode.java
@@ -1,50 +1,59 @@
+package linkedlistshisto;
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
 //Name -
 
-public class HistoNode
-{
+public class HistoNode {
 	private char letter;
 	private int letterCount;
 	private HistoNode next;
 
-	public HistoNode(char let, int cnt, HistoNode n)
-	{
-		letter=let;
-		letterCount=cnt;
-		next=n;
+	/* Parameterized constructor 
+	 * @param let	the character being referenced in the node
+	 * @param cnt	how many times let occurs
+	 * @param n 	the next node in the list */
+	public HistoNode(char let, int cnt, HistoNode n) {
+		letter = let;
+		letterCount = cnt;
+		next = n;
 	}
 
-	public char getLetter()
-	{
+	/* Returns letter 
+	 * @return		let */
+	public char getLetter() {
 		return letter;
 	}
 
-	public int getLetterCount()
-	{
+	/* Returns letterCount 
+	 * @return		letterCount */
+	public int getLetterCount() {
 		return letterCount;
 	}
 
-	public HistoNode getNext()
-	{
+	/* Returns the next node in the list 
+	 * @return		next */
+	public HistoNode getNext() {
 	   return next;
 	}
 
-	public void setLetter(char let)
-	{
-		letter=let;
+	/* Changes the value of let 
+	 * @param		new let value */
+	public void setLetter(char let) {
+		letter = let;
 	}
 
-	public void setLetterCount(int cnt)
-	{
-		letterCount=cnt;
+	/* Changes the value of cnt 
+	 * @param		new cnt value */
+	public void setLetterCount(int cnt) {
+		letterCount = cnt;
 	}
 
-	public void setNext(HistoNode n)
-	{
+	/* Changes the next node in the list 
+	 * @param		new next node */
+	public void setNext(HistoNode n) {
 		next = n;
 	}
+	
 }
-
-
